872139aea382acb8d1a11f91a08568f6b08947a7
Seongjin Cho <42935823+seongjinemong@users.noreply.github.com>
2024-10-29 21:01:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# React + Vite + Zustand
+## 프론트 개발을 빠르게 시작하기 위해 만든 템플릿입니다
+이 세상 모든 개발자들 화이팅!
+
+### 실행 방법
+```
+npm install
+npm run dev
+```
+
---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Easteregg from "./pages/Easteregg.jsx";
+// 토스트 알림 띄우기 위해 쓰는 라이브러리
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/easteregg",
+    element: <Easteregg />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* 알림 설정 */}
+    <ToastContainer
+      position="bottom-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      theme="dark"
+      transition={Slide}
+    />
+    <RouterProvider router={router} />
+  </StrictMode>
+);
