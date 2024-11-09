5245de360af5c6332fffd70fae1d3c284f3e669a
홍성문 <2020041412@hanyang.ac.kr>
2024-11-09 11:37:36 +0900

    feat: TimespaceButton 컴포넌트 제작

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,43 @@
 import { useState } from "react";
 // 라우팅
 import { useNavigate } from "react-router-dom";
-// 토스트 알림
-import { toast } from "react-toastify";
-// ???
-import messages from "./lib/messages";
 
-// App 컴포넌트
+import TimespaceButton from "./components/TimespaceButton/TimespaceButton";
+
+
+
 function App() {
   const [count, setCount] = useState(0);
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    setCount(count + 1);
-
-    if (count >= messages.length) navigate("/easteregg");
-    else toast.info(messages[count]);
-  };
-
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-4">
-     
+    <div style={{ padding: '20px' }}>
+      <h1>Customized Timespace Buttons</h1>
+
       <h1 className="font-bold text-7xl">FORIF-REACT-2024</h1>
 
-      <p className="text-xl">이번 프로젝트 화이팅!</p>
+      {/* 기본 버튼 */}
+      <TimespaceButton
+        label="기본 버튼"
+        onClick={() => alert('Basic Button Clicked')}
+        styleType="timespace"
+        style={{ marginBottom: '200px' }}
+      />
 
-      <button
-        className="rounded-md bg-gray-700 text-white p-2 hover:bg-gray-800 border-[1px] border-transparent hover:border-white transition-all duration-100 px-3"
-        onClick={handleClick}
-      >
-        눌러보세요
-      </button>
+      {/* 이중 테두리 스타일 버튼 */}
+      <TimespaceButton
+        label="이중 테두리 버튼"
+        onClick={() => alert('Double Border Button Clicked')}
+        styleType="timespace2"
+      />
+
+      {/* Danger 스타일 버튼 */}
+      <TimespaceButton
+        label="Danger 버튼"
+        onClick={() => alert('Danger Button Clicked')}
+        styleType="timespace3"
+      />
+      
     </div>
   );
 }
